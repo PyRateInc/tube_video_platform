aa6f41a48b8f28b43104cdf694e60f4f258c6102
Nemo <nemo@localhost.localdomain>
2020-12-14 18:17:48 +0000

    Changes

--- youtube_python/settings.py
+++ youtube_python/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'youtube_python.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['youtube'],
+        'DIRS': ['youtube', os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,3 +108,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_ROOT=os.path.join(BASE_DIR, 'static_root')
+MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
