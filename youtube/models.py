59a3e230fb62031460f1f3ce298ee60adf5bb105
Nemo <nemo@localhost.localdomain>
2021-01-11 18:10:56 +0000

    Testing Cloudinary

--- youtube/models.py
+++ youtube/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 
@@ -8,6 +9,8 @@ class Video(models.Model):
     path = models.CharField(max_length=60)
     datetime = models.DateTimeField(auto_now=True, blank=False, null=False) #todo: auto_now=True
     user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    #Just trying upload to cloudnary.
+    videoStorage = CloudinaryField("video")
 
 class Comment(models.Model):
     text = models.TextField(max_length=300)
